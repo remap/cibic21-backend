87401426e6ffa1e5d4090fd29adfccfa5ba5b334
jefft0 <jeff@thefirst.org>
2022-07-18 09:58:49 -0700

    ambda/ride-geo-data-api-access: Fix SQL indentation. Same functionality.

--- lambda/ride-geo-data-api-access/lambda_function.py
+++ lambda/ride-geo-data-api-access/lambda_function.py
@@ -51,19 +51,19 @@ def fetchRide(rideId):
     sql = """
             SET TIME ZONE 'America/Los_Angeles';
             SELECT json_build_object(
-                    'type', 'FeatureCollection',
-                    'features', array_to_json(feature_list),
-                    'properties', json_build_object(
-                                    'rideId', rid,
-                                    'startTime', start_time,
-                                    'endTime', end_time,
-                                    'userId', user_id,
-                                    'role', role,
-                                    'flow', flow,
-                                    'flowName', flow_name,
-                                    'flowIsToWork', flow_is_to_work
-                                  )
-                    )
+                     'type', 'FeatureCollection',
+                     'features', array_to_json(feature_list),
+                     'properties', json_build_object(
+                                     'rideId', rid,
+                                     'startTime', start_time,
+                                     'endTime', end_time,
+                                     'userId', user_id,
+                                     'role', role,
+                                     'flow', flow,
+                                     'flowName', flow_name,
+                                     'flowIsToWork', flow_is_to_work
+                                   )
+                   )
             FROM (SELECT array[json_build_object(
                                 'type', 'Feature',
                                 'geometry', ST_AsGeoJSON(start_zone)::json
@@ -88,12 +88,14 @@ def fetchRide(rideId):
                                ride."flow" AS flow,
                                ride."flowName" AS flow_name,
                                ride."flowIsToWork" AS flow_is_to_work
-                        FROM {} AS ride
-                        LEFT JOIN (SELECT * FROM {} WHERE zone = 'main') AS wp
-                        ON ride."rideId" = wp."rideId"
-                        WHERE ride."rideId" = '{}'
-                          AND wp.zone = 'main'
-                        GROUP BY ride."rideId") AS geo) AS feature_collection;
+                         FROM {} AS ride
+                         LEFT JOIN (SELECT * FROM {} WHERE zone = 'main') AS wp
+                         ON ride."rideId" = wp."rideId"
+                         WHERE ride."rideId" = '{}'
+                           AND wp.zone = 'main'
+                         GROUP BY ride."rideId"
+                       ) AS geo
+                 ) AS feature_collection;
           """.format(CibicResources.Postgres.Rides, CibicResources.Postgres.WaypointsRaw, rideId)
     conn = psycopg2.connect(host=pgServer, database=pgDbName,
                                             user=pgUsername, password=pgPassword)
@@ -141,19 +143,19 @@ def queryRidesRich(startTime, endTime):
     sql = """
             SET TIME ZONE 'America/Los_Angeles';
             SELECT json_build_object(
-                    'type', 'FeatureCollection',
-                    'features', array_to_json(feature_list),
-                    'properties', json_build_object(
-                                    'rideId', rid,
-                                    'startTime', start_time,
-                                    'endTime', end_time,
-                                    'userId', user_id,
-                                    'role', role,
-                                    'flow', flow,
-                                    'flowName', flow_name,
-                                    'flowIsToWork', flow_is_to_work
-                                  )
-                    )
+                     'type', 'FeatureCollection',
+                     'features', array_to_json(feature_list),
+                     'properties', json_build_object(
+                                     'rideId', rid,
+                                     'startTime', start_time,
+                                     'endTime', end_time,
+                                     'userId', user_id,
+                                     'role', role,
+                                     'flow', flow,
+                                     'flowName', flow_name,
+                                     'flowIsToWork', flow_is_to_work
+                                   )
+                   )
             FROM (SELECT array[json_build_object(
                                 'type', 'Feature',
                                 'geometry', ST_AsGeoJSON(start_zone)::json
@@ -178,12 +180,14 @@ def queryRidesRich(startTime, endTime):
                                ride."flow" AS flow,
                                ride."flowName" AS flow_name,
                                ride."flowIsToWork" AS flow_is_to_work
-                        FROM {0} AS ride
-                        LEFT JOIN (SELECT * FROM {1} WHERE zone = 'main') AS wp
-                        ON ride."rideId" = wp."rideId"
-                        WHERE ride."startTime" BETWEEN '{2}' AND '{3}'
-                        GROUP BY ride."rideId"
-						            ORDER BY ride."startTime" DESC) AS geo) AS feature_collection;
+                         FROM {0} AS ride
+                         LEFT JOIN (SELECT * FROM {1} WHERE zone = 'main') AS wp
+                         ON ride."rideId" = wp."rideId"
+                         WHERE ride."startTime" BETWEEN '{2}' AND '{3}'
+                         GROUP BY ride."rideId"
+						             ORDER BY ride."startTime" DESC
+                       ) AS geo
+                 ) AS feature_collection;
           """.format(CibicResources.Postgres.Rides, CibicResources.Postgres.WaypointsRaw,
                     startTime.astimezone().strftime("%Y-%m-%d %H:%M:%S%z"),
                     endTime.astimezone().strftime("%Y-%m-%d %H:%M:%S%z"))
