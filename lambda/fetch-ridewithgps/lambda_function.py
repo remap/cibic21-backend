2f2c0d746dfba9fe3acda09f28d5690c70d04ec8
jefft0 <jeff@thefirst.org>
2022-10-10 10:32:30 -0700

    lambda/fetch-ridewithgps: Get users from the enrollments table.

--- lambda/fetch-ridewithgps/lambda_function.py
+++ lambda/fetch-ridewithgps/lambda_function.py
@@ -1,11 +1,16 @@
-# This Lambda is for fetching user and ride info from the RideWithGPS API.
+# This Lambda is for fetching ride info from the RideWithGPS API.
 
+import psycopg2
 from common.cibic_common import *
 
 # Python 3.8 lambda environment does not have requests https://stackoverflow.com/questions/58952947/import-requests-on-aws-lambda-for-python-3-8
 # for a fix using Lambda Layers, see https://dev.to/razcodes/how-to-create-a-lambda-layer-in-aws-106m
 import requests
 
+pgServer = os.environ['ENV_VAR_POSTGRES_SERVER']
+pgDbName = os.environ['ENV_VAR_POSTGRES_DB']
+pgUsername = os.environ['ENV_VAR_POSTGRES_USER']
+pgPassword = os.environ['ENV_VAR_POSTGRES_PASSWORD']
 clubId = os.environ['ENV_VAR_RWGPS_CLUB_ID']
 apiKey = os.environ['ENV_VAR_RWGPS_API_KEY']
 authToken = os.environ['ENV_VAR_RWGPS_AUTH_TOKEN']
@@ -20,16 +25,12 @@ def lambda_handler(event, context):
         for id, route in routes.items():
             print("Route " + str(id) + ' "' + str(route.get('name')) + '"')
 
-        users = fetchUsers()
-        for userId, user in users.items():
-            if not user.get('active') == True:
-                continue
-            if user.get('approved_at') == None:
-                continue
-            if not 'user' in user:
-                continue
+        conn = psycopg2.connect(host=pgServer, database=pgDbName,
+                                user=pgUsername, password=pgPassword)
+        cur = conn.cursor()
 
-            displayName = str(user['user'].get('display_name'))
+        users = queryActiveUsers(cur, 'Buenos Aires')
+        for userId, user in users.items():
             trips = fetchUserTrips(userId)
             routeInfo = ""
             if len(trips) <= 5:
@@ -42,8 +43,12 @@ def lambda_handler(event, context):
                               ' route ' + str(extra.get('id')))
                             break
 
-            print("User " + str(userId) + ": " + displayName + ", " +
+            print("User " + str(userId) + ": " + str(user.get('displayName')) + ", " +
                   str(len(trips)) + " trips" + routeInfo)
+
+        conn.commit()
+        cur.close()
+
         requestReply = processedReply()
     except:
         err = reportError()
@@ -69,22 +74,26 @@ def fetchRoutes():
     else:
         raise ValueError('RideWithGPS API request for routes failed with code {}'.format(response.status_code))
 
-def fetchUsers():
+def queryActiveUsers(cur, region):
     """
-    Fetch all the users for clubId (defined by the environment variable) from
-    the RideWithGPS API. Return a dict where the key is the user ID and the
-    value is the user JSON. Throw an exception for error.
+    Query the enrollments table for all active users in the region. Return a
+    dict where the key is the user ID and the value is a JSON with the info.
     """
-    response = requests.get(
-      'https://ridewithgps.com/clubs/' + str(clubId) + '/table_members.json?version=3&apikey=' +
-      apiKey + '&auth_token=' + authToken)
-    if response.status_code/100 == 2:
-        result = {}
-        for user in response.json():
-            result[user['user_id']] = user
-        return result
-    else:
-        raise ValueError('RideWithGPS API request for users failed with code {}'.format(response.status_code))
+    sql = """
+      SELECT "userId", role, "displayName", email
+      FROM {0}
+      WHERE active = TRUE AND region = '{1}'
+    """.format(CibicResources.Postgres.UserEnrollments, region)
+    cur.execute(sql)
+    result = {}
+    for user in cur.fetchall():
+        result[user[0]] = {
+            'role': user[1],
+            'displayName': user[2],
+            'email': user[3]
+        }
+
+    return result
 
 def fetchUserTrips(userId):
     """
