b4c09cb01c47c2613622dec0af67d698e05c35a3
jefft0 <jeff@thefirst.org>
2022-11-29 02:37:41 -0800

    lambda/user-ride-statistics-api: Update the sql to use a subquery (same functionality).

--- lambda/user-ride-statistics-api/lambda_function.py
+++ lambda/user-ride-statistics-api/lambda_function.py
@@ -29,27 +29,6 @@ def lambda_handler(event, context):
             else:
                 print('no users matching query parameters found')
                 return lambdaReply(404, 'not found')
-
-        if event['requestContext']['resourcePath'] == '/query':
-            if 'startTime' in event['queryStringParameters'] and 'endTime' in event['queryStringParameters']:
-                startTime = parseDatetime(event['queryStringParameters']['startTime'])
-                endTime = parseDatetime(event['queryStringParameters']['endTime'])
-
-                if not startTime or not endTime:
-                    return lambdaReply(420, 'bad format for startTime/endTime parameters')
-
-                region = event['queryStringParameters'].get('region')
-                organization = event['queryStringParameters'].get('organization')
-                requireFlow = ('requireFlow' in event['queryStringParameters'])
-
-                if 'idsOnly' in event['queryStringParameters']:
-                    rides = queryRidesSimple(startTime, endTime, region, organization, requireFlow)
-                else:
-                    rides = queryRidesRich(startTime, endTime, region, organization, requireFlow)
-                print('fetched {} rides'.format(len(rides)))
-                return lambdaReply(200, rides)
-            else:
-                return malformedMessageReply()
     except:
         err = reportError()
         print('caught exception:', sys.exc_info()[0])
@@ -66,9 +45,9 @@ def fetchUserRideStatistics(region, organization, startTime, endTime):
     """
     extraUsersWhere = ''
     if region != None:
-        extraUsersWhere += " AND users.region = '{}'".format(region)
+        extraUsersWhere += " AND region = '{}'".format(region)
     if organization != None:
-        extraUsersWhere += " AND users.organization = '{}'".format(organization)
+        extraUsersWhere += " AND organization = '{}'".format(organization)
 
     extraRidesWhere = ''
     if startTime != None:
@@ -80,21 +59,23 @@ def fetchUserRideStatistics(region, organization, startTime, endTime):
 
     sql = """
 SELECT json_build_object(
-         'userId', users."userId",
-         'role', users.role,
-         'displayName', users."displayName",
-         'email', users.email,
-         'outwardFlowId', users."outwardFlowId",
-         'outwardFlowName', users."outwardFlowName",
-         'returnFlowId', users."returnFlowId",
-         'returnFlowName', users."returnFlowName",
+         'userId', "userId",
+         'role', role,
+         'displayName', "displayName",
+         'email', email,
+         'outwardFlowId', "outwardFlowId",
+         'outwardFlowName', "outwardFlowName",
+         'returnFlowId', "returnFlowId",
+         'returnFlowName', "returnFlowName",
          'totalRides', (SELECT COUNT(*) FROM {1} AS rides
                         WHERE rides."userId" = users."userId" AND
                         rides.flow IS NOT NULL {3})
        )
-FROM {0} as users
-WHERE users.active = True AND users.deleted = False {2}
-ORDER BY users."userId";
+FROM (SELECT "userId", role, "displayName", email,
+      "outwardFlowId", "outwardFlowName", "returnFlowId", "returnFlowName"
+      FROM {0}
+      WHERE active = True AND deleted = False {2}
+      ORDER BY "userId") AS users;
           """.format(CibicResources.Postgres.UserEnrollments,
                      CibicResources.Postgres.Rides, extraUsersWhere, extraRidesWhere)
     conn = psycopg2.connect(host=pgServer, database=pgDbName,
